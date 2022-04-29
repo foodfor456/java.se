c0a03d8a1cde71813bf200b7a25a2c9bc38cac83
foodfor456 <foodfor456@naver.com>
2022-04-29 17:04:04 +0900

    자바 5일차 : Random 예제

---
+++ java/src/day5/Ex2_Random.java
@@ -0,0 +1,23 @@
+package day5;
+
+public class Ex2_Random {
+
+	public static void main(String[] args) {
+		/* 0 <= Math.random() < 1 실수
+		 * 0 * (max - min+1) <= Math.random() * (max-min+1) < * (max - min+1)
+		 * 0 <= Math.random() * (max-min+1) + min
+		 * 0 + min <= Math.random() *  (max-min+1) + min < (max - min+1) + min
+		 * 
+		 * 
+		 * */
+		int min = 1, max = 10;
+		int r;
+		for(int i = 1; i<=10 ; i++) {
+			r = (int)(Math.random() * (max-min+1) + min);
+			System.out.print(r + " ");
+					
+		}
+			
+		}
+
+}
