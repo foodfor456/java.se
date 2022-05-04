d43bfbdd7a47cd0ad437055aa79788b81c3f3668
foodfor456 <foodfor456@naver.com>
2022-05-04 12:36:35 +0900

    자바 7일차 : 배열 예제 sum

---
+++ java/src/day7/Ex1_Array1.java
@@ -0,0 +1,24 @@
+package day7;
+
+public class Ex1_Array1 {
+
+	public static void main(String[] args) {
+		/* 5개짜리 배열을 만들고, 0번지에 1, 1번지에 2, ... 4번지에 5를 저장하고 출력하는 코드를 작성하세요.
+		 * */
+		int arr[] = new int [5];
+		for(int i = 0 ; i < arr.length; i++) {
+			arr[i] = i+1;
+			System.out.println(arr[i]);
+		}
+		int num[] = new int[5];
+		int sum = 1;
+		for (int i = 0; i<num.length;i++) {
+			num[i]=sum;
+			System.out.println(num[i]);
+			sum++;
+	}
+	
+		
+	}
+
+}
