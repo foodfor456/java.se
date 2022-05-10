f492aa7c17aa43cf4dd29ede79f5fbb58b9d520c
foodfor456 <foodfor456@naver.com>
2022-05-10 09:32:52 +0900

    자바 7일차 : 배열 로또 예제

---
+++ java/src/day7/Ex6_Array_Lotto.java
@@ -0,0 +1,161 @@
+package day7;
+
+import java.util.Scanner;
+
+public class Ex6_Array_Lotto {
+
+	public static void main(String[] args) {
+		/* 1~45사이의 중복되지 않은 수 7개를 배열에 저장하는 코드를 작성하세요.
+		 * */
+		int lotto[] = new int[7]; // 6번지가 보너스 번호
+		int count = 0;
+		int count1 = 0;
+		int count2 = 0;
+		for ( ; count < 7 ; ) {
+			int r = (int)(Math.random() *45 + 1);
+			int i;
+			for(i=0 ; i<count ; i++) {
+				if(r == lotto[i]) {
+					break;
+				}
+			}
+			if(count==6) {
+				lotto[count++] = r;
+				System.out.print(" "+"보너스 번호 " + r);
+			}
+			if(count==i && count!=6) {
+				lotto[count++] = r;
+				System.out.print(" " + r + " ");
+			}
+		}
+		System.out.println();
+		//사용자 번호 : 1~45 사이의 정수 6개를 입력받아 user배열에 저장하는 코드를 작성하세요.
+		Scanner scan = new Scanner(System.in);
+		int user[] = new int[6];
+		System.out.println("숫자를 입력하세요 : ");
+		for(int i = 0 ; i < user.length; i++) {
+			user[i] = scan.nextInt();
+		}
+		for(int i = 0 ; i<user.length; i++) {
+		for(int j = 0 ; j<user.length; j++) {
+			if(lotto[i]==user[j]) {
+				count1++;
+			}if(lotto[6]==user[j])
+				count2++;
+		}
+		}
+		if(count1 == 6) {
+			System.out.println("1등입니다.");
+		}else if (count1 == 5 && count2 > 0) {
+			System.out.println("2등입니다.");
+		}else if (count1 == 5 && count2 == 0) {
+			System.out.println("3등입니다.");
+		}else if (count1 == 4) {
+			System.out.println("4등입니다.");
+		}else if (count1 == 3) {
+			System.out.println("5등입니다.");
+		}else{
+			System.out.println("꽝 입니다.");
+		}
+		
+		
+		
+		
+		/* 로또번호와 사용자 번호를 이용하여 등수를 출력하는 코드를 작성하세요.
+		 * 1등 : 6개 일치
+		 * 2등 : 5개 + 보너스(6번지) 번호 일지
+		 * 3등 : 5개 일치
+		 * 4등 : 4개 일치
+		 * 5등 : 3개 일치
+		 * 꽝  : 그 외
+		 * */
+		
+		
+		
+		scan.close();
+		
+		/*int lotto[] = new int[7];//6번지가 보너스 번호 
+		int count = 0;//배열에 저장된 중복되지 않은 숫자의 개수
+		//로또 당첨번호
+		for( ; count < 7 ; ) {
+			int r = (int)(Math.random()*45 + 1);
+			int i;
+			for(i=0 ;i<count ; i++ ) {
+				if(r == lotto[i]) {
+					break;
+				}
+			}
+			if(i == count) {
+				lotto[count] = r;
+				System.out.print(r + " ");
+				
+			}
+		}
+		System.out.println();
+		int user[] = new int[6];
+		Scanner scan = new Scanner(System.in);
+		System.out.print("로또 번호 입력 : ");
+		for(int i = 0; i<user.length; i++) {
+			user[i] = scan.nextInt();
+		}
+		int count2 = 0;//일치하는 번호의 갯수
+		for(int i = 0; i<user.length; i++) {
+			for(int j = 0; j<user.length; j++) {
+				if(lotto[i] == user[j]) {
+					count2++;
+				}
+			}
+		}
+		switch(count2) {
+		case 6:
+			System.out.println("1등");
+			break;
+		case 5:
+			int k;
+			for(k=0; k<user.length; k++) {
+				if(user[k] == lotto[6]) {
+					break;
+				}
+			}
+			if(k == user.length) {
+				System.out.println("3등");
+			}else {
+				System.out.println("2등");
+			}
+			break;
+		case 4:
+			System.out.println("4등");
+			break;
+		case 3:
+			System.out.println("5등");
+			break;
+		default:
+			System.out.println("꽝");
+		}
+		scan.close();
+		/*for( ;count < 7; ) {
+			int r = (int)(Math.random() * (max-min+1) + min);
+			int i = 0;
+		for( ;i <count ;i++) {
+			if(r==lotto[i]) {
+				break;
+			}
+			}
+			if(count == i && count != 7) {
+			lotto[count] = r;
+			System.out.println(lotto[count]);	
+			count++;
+				
+			}if(count == 7) {
+			lotto[count] = r;
+			System.out.println("보너스 번호 : " + lotto[count]);
+			}
+		
+		}
+		*/
+		
+		
+		
+	}
+
+}
