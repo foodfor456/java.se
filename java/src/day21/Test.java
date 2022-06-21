f0719986a3d0298500c70f28970873bc3fa6c22a
foodfor456 <foodfor456@naver.com>
2022-06-21 17:14:10 +0900

    자바 21일차 : test2

---
+++ java/src/day21/Test.java
@@ -0,0 +1,13 @@
+package day21;
+
+public class Test {
+
+	public static void main(String[] args) {
+		int sum = 0;
+		int i;
+		for(i = 1; i <= 10; i++) {
+			sum += i;
+		}
+		System.out.println(sum);
+	}
+}
